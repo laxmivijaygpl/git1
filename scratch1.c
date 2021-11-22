7de05d429456da1cd613d7fa8537c0af04821a9c
vijayahemansh2019 <vijayahemansh2019@gmail.com>
2021-11-21 21:36:59 -0800

    added sum of two numbers

---
+++ scratch1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+   int a, b, sum;
+
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b);
+
+   sum = a + b;
+
+   printf("Sum : %d", sum);
+
+   return(0);
+}
+Output :
+
+Enter two no: 5 6
+Sum : 11
+1
+2
+Enter two no: 5 6
+Sum : 11
---
+++ scratch2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+   int a, b, sum;
+
+   printf("\nEnter two no: ");
+   scanf("%d %d", &a, &b);
+
+   sum = a + b;
+
+   printf("Sum : %d", sum);
+
+   return(0);
+}
+Output :
+
+Enter two no: 11 22
+Sum : 33
+
+Enter two no: 5 6
+Sum : 11
